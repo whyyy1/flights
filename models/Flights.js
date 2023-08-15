150fe3096cb891e9f8e9afdd07700eecd41347fb
whyyy1 <dysarttieran@gmail.com>
2023-08-15 11:49:04 -0600

    schemas

--- models/Flights.js
+++ models/Flights.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
-// this Schema acts as a blueprint for our data
+//PARENT SCHMEA
 const flightSchema = new Schema({
     airline: {
         type: String,
@@ -10,6 +10,12 @@ const flightSchema = new Schema({
         
         required: true
     },
+    airport: {
+        type: String,
+        enum : ['AUS', 'DAL', 'LAX', 'SAN' , 'SEA'],
+        default: 'SAN',
+    },
+
     flightNo: {
         type: Number,
         min: 10,
@@ -20,8 +26,22 @@ const flightSchema = new Schema({
         type: Date,
         default: new Date().getTime()  ,
         required: true
-    }
+    }, 
+    destinations: destinationSchema
 })
+
+//CHILD SCHEMA
+const destinationSchema  = new Schema({
+    airport: {
+        type: String,
+        enum : ['AUS', 'DAL', 'LAX', 'SAN' , 'SEA'],
+        
+    },
+    arrival: {
+        type: Date
+    },
+})
+
 
 // 1st argument is the collection, 2nd is the Schema (blueprint) 
 const Flights = mongoose.model('flights', flightSchema)
